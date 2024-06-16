19d02a990b55a8d1f3c9d67ac6a47b123fcf9fc7
Jacky Jiang <t83714@gmail.com>
2024-06-16 20:45:12 +1000

    make LLM run as service worker

--- examples/chrome-extension-webgpu-service-worker/src/background.ts
+++ examples/chrome-extension-webgpu-service-worker/src/background.ts
@@ -1,10 +1,20 @@
 import { ExtensionServiceWorkerMLCEngineHandler } from "@mlc-ai/web-llm";
-
 // Hookup an engine to a service worker handler
 let handler;
 
-chrome.runtime.onConnect.addListener(function (port) {
-  console.assert(port.name === "web_llm_service_worker");
+chrome.runtime.onConnectExternal.addListener(function (port) {
+  if (port.name === "keep_alive") {
+    port.onMessage.addListener(() =>
+      port.postMessage({
+        timestamp: new Date().toISOString(),
+      }),
+    );
+    return;
+  }
+  if (port.name !== "web_llm_service_worker") {
+    console.warn("New connection from unknown port name: " + port.name);
+    return;
+  }
   if (handler === undefined) {
     handler = new ExtensionServiceWorkerMLCEngineHandler(port);
   } else {
